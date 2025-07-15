e0cb51d1d94fad51fd0153b96ed31be971c001c0
dGilli <me@dennisgilli.com>
2025-07-15 11:08:31 +0200

    change hihat sample and clap delay to mini notation

--- maniac.js
+++ maniac.js
@@ -11,10 +11,8 @@ setcpm(155.2/2)
 $:stack(
   s("sd -"),
   s("<-!4 rim@2 -!2 rim!3 ->*4").gain(.3),
-  s("hh*4").gain(.25),
-  s("<-!7 cp*4 cp cp*4 cp ->*4").gain(.1).lpf(8000).delay(.1).dt(.1).pan(0.2),
+  s("hh:2*4").gain(.25),
+  s("<-!7 cp*4 cp cp*4 cp ->*4").gain(.1).lpf(8000).delay(".1:.1").pan(0.2),
   s("<cb cb*3 cb -!8 cb*3>*4").gain(.12).pan(0.8),
   s("bd*2").room(.03).rsize(2),
-).bank("linndrum")
-._punchcard()
-
+).bank("linndrum")._punchcard()
