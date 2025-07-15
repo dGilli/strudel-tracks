880b8188b18ac81e0e9dfb2d29ea9c86a2a57b09
dGilli <me@dennisgilli.com>
2025-07-15 11:11:10 +0200

    add simmons track snare drum and low tom

--- maniac.js
+++ maniac.js
@@ -8,7 +8,7 @@ script @by https://github.com/dGilli
 
 setcpm(155.2/2)
 
-$:stack(
+LINNDRUM:stack(
   s("sd -"),
   s("<-!4 rim@2 -!2 rim!3 ->*4").gain(.3),
   s("hh:2*4").gain(.25),
@@ -16,3 +16,9 @@ $:stack(
   s("<cb cb*3 cb -!8 cb*3>*4").gain(.12).pan(0.8),
   s("bd*2").room(.03).rsize(2),
 ).bank("linndrum")._punchcard()
+
+SIMMONS:stack(
+  s("-!2 sd:14 -, <-!11 sd:14>*4"),
+  s("<-!9 lt!3>*4").note("<40 36 33>*4").when("<1 0 1>*4", x=>x.delay(".1:.03:.08")),
+).bank("simmonssds5")._punchcard()
+
